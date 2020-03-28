96f0fb545562e8bf0e5f5152ca8e8b7f6f767fbb
bread <claire.raud@gmail.com>
2020-03-27 23:55:52 -0500

    optimize

--- server.js
+++ server.js
@@ -9,20 +9,6 @@ const port = process.env.APP_PORT
 const darkSkyApiSecret = process.env.DARKSKY_API_SECRET
 const ipstackApiSecret = process.env.IPSTACK_API_SECRET
 
-const app = express()
-
-app.use(express.static('public'))
-
-app.get('/weather', asyncHandler(async (req, res) => 
-    res.json(await publicIp.v4()
-        .then(getGeoIp)
-        .then(getWeather)
-        .then(formatAllData)
-        .then(([time, temperature, summary]) => {time, temperature, summary})
-        .catch(e => console.error))
-))
-app.listen(port, () => console.log(`Weather service listening on port ${port}`))
-
 const getGeoIp = ip => 
     axios.get(`http://api.ipstack.com/${ip}?access_key=${ipstackApiSecret}`)
         .catch(err => console.error(err))
@@ -31,7 +17,8 @@ const getGeoIp = ip =>
 const getWeather = ({latitude,longitude}) => 
     axios.get(`https://api.darksky.net/forecast/${darkSkyApiSecret}/${latitude},${longitude}`)
         .catch(err => console.error(err))
-        .then(({data}) => data)
+        .then(({data}) => { console.log(data); return data })
+        // .then(({data}) => data)
 
 const formatOutput = text => new Promise((resolve, reject) => 
     figlet(text, (err, data) => {
@@ -41,7 +28,30 @@ const formatOutput = text => new Promise((resolve, reject) =>
 
 const formatAllData = weather => 
     Promise.all([
-        formatOutput(moment().format('hh : mm : ss A')),
+        formatOutput(`updated ${moment().format('hh : mm : ss A')}`),
         formatOutput(`${weather.currently.temperature}°F`),
         formatOutput(weather.currently.summary)
     ])
+
+
+const startServer = async () => {
+    const geoIp = await publicIp.v4()
+        .then(getGeoIp)
+
+    const app = express()
+
+    app.use(express.static('public'))
+    
+    app.get('/weather', asyncHandler(async (req, res) => 
+        res.json(
+            getWeather(geoIp)
+            .then(formatAllData)
+            .then(([time, temperature, summary]) => {time, temperature, summary})
+            .catch(e => console.error))
+    ))
+
+    return app
+}
+
+startServer().then(app => app.listen(port, () => console.log(`Weather service listening on port ${port}`)))
+
