3b015d5bf7fecaa2b3bed7a5a01019bb5c843ce7
bread <claire.raud@gmail.com>
2020-03-29 00:26:54 -0500

    Cache geoip information to avoid rate limiting

--- src/lib/Geoip.ts
+++ src/lib/Geoip.ts
@@ -3,15 +3,19 @@ const publicIp = require('public-ip')
 
 export default class Geoip {
     private apiSecret;
+    private geoIp;
 
     constructor(apiSecret) {
         this.apiSecret = apiSecret
     }
 
     async getGeoip() {
-        return publicIp.v4()
-        .then(ip => axios.get(`http://api.ipstack.com/${ip}?access_key=${this.apiSecret}`)
-        .then(({data}) => data))
-        .catch(err => console.error(err))
+        if (!this.geoIp) {
+            this.geoIp = await publicIp.v4()
+                .then(ip => axios.get(`http://api.ipstack.com/${ip}?access_key=${this.apiSecret}`)
+                .then(({data}) => data))
+                .catch(err => console.error(err))
+        }
+        return this.geoIp
     }
 }
